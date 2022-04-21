63d5c34fb1e0191d5d8d508f48998aa8f6833009
Richard A <richard@main-vision.com>
2022-04-21 18:16:10 +0200

    finish tic tac toe game

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -71,3 +71,55 @@ int main()
   cin.get();
   return 0;
 }
+
+int checkwin()
+{
+  if (square[1] == square[2] && square[2] == square[3])
+
+    return 1;
+  else if (square[4] == square[5] && square[5] == square[6])
+
+    return 1;
+  else if (square[7] == square[8] && square[8] == square[9])
+
+    return 1;
+  else if (square[1] == square[4] && square[4] == square[7])
+
+    return 1;
+  else if (square[2] == square[5] && square[5] == square[8])
+
+    return 1;
+  else if (square[3] == square[6] && square[6] == square[9])
+
+    return 1;
+  else if (square[1] == square[5] && square[5] == square[9])
+
+    return 1;
+  else if (square[3] == square[5] && square[5] == square[7])
+
+    return 1;
+  else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+
+    return 0;
+  else
+
+    return -1;
+}
+
+void board()
+{
+  system("cls");
+  cout<<"\n\n\tTic Tac Toe\n\n";
+  cout<<"Player 1 (X)  -  Player 2 (O)\n\n";
+  cout<<"     |     |     \n";
+  cout<<"  "<<square[1]<<"  |  "<<square[2]<<"  |  "<<square[3]<<"  \n";
+  cout<<"_____|_____|_____\n";
+  cout<<"     |     |     \n";
+  cout<<"  "<<square[4]<<"  |  "<<square[5]<<"  |  "<<square[6]<<"  \n";
+  cout<<"_____|_____|_____\n";
+  cout<<"     |     |     \n";
+  cout<<"  "<<square[7]<<"  |  "<<square[8]<<"  |  "<<square[9]<<"  \n";
+  cout<<"     |     |     \n\n";
+}
+
+// http://www.cppforschool.com/project/tic-tac-toe-project.html
