0ea669c1f80ebeda60088bc847e2f8be8f450650
Richard A <richard@main-vision.com>
2022-04-22 17:56:27 +0200

    cpp variable examples

--- name.cpp
+++ name.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-using namespace std;
+int a, b = 5;
 
 int main(){
-    string name;
-    cout << "Do you remember your name?" << endl;
-    cin >> name;
-    cout << "Hello, happy to meet you, " << name << endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    cout << a << endl;
+    cout << b << endl;
+    cout << "flag = " << my_flag << endl;
+    my_flag = true;
+    cout << "flag = " << my_flag << endl;
+    cout << "a+b = " << a+b << endl;
+    cout << "b-a = " << b-a << endl;
+    unsigned int positive;
+    positive = b-a;
+    cout << "b-a (unsigned) = " << positive << endl;
     return 0;
 }
