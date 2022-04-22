24ef5ccc924c3396b556863a47b5c03eaf8fd222
Richard A <richard@main-vision.com>
2022-04-22 17:25:35 +0200

    simple in out challenge

---
+++ ioname.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string nickname;
+    cin>> nickname;
+    cout<<"\n\n\tHey "<<nickname<<"!"<<endl;
+    cout<<"\n\tWelcome to the C++ Experience!"<<endl;
+    cout<<"\n\tWe will see how long I struggle with C++."<<endl;
+    return(0);
+}
